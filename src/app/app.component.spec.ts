4742cd5cfe5e2afd42b46f8d46603d3c3a7e7219
marcbudd <marcbuddemeier@gmail.com>
2022-12-13 18:04:41 +0100

    Change project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ProjectForTielsch'`, () => {
+  it(`should have as title 'frontend-P2Cinema'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ProjectForTielsch');
+    expect(app.title).toEqual('frontend-P2Cinema');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ProjectForTielsch app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('frontend-P2Cinema app is running!');
   });
 });
--- src/app/specials/specials.component.ts
+++ src/app/specials/specials.component.ts
@@ -7,8 +7,8 @@ import { Component } from '@angular/core';
 })
 export class SpecialsComponent {
     datalistSpecials:any = [
-      {specialname:"Kostümevent",source:"assets/img/Kostümevent.jpg",description: "Beschere dir und allen anderen ein einzigartiges Erlebnis. Komme verkleidet in einem Kostüm zu deinem Film und erhalte ein Gratis Menü."},
-                {specialname:"Popcornspecial",source:"assets/img/Popcornspecial.jpg",description:"Riesenpopcornangebot: Wenn ihr mit 5 Personen kommt bekommt ihr einen großen Popcorneimer geschenkt"},
-                {specialname:"The Night is our",source:"assets/img/movienight.jpg",description:"Filmmarathon über die ganze Nacht. Tauche mit vielen anderen in die fantstische Welt der Filme ein. Die Filme werden von den Zuschauern durch Abszimmung bestimmt, der Preis beträgt 100 Euro und die Tickets sind nur an der Kasse erhältlich. Die Tickets sind nur für den jeweiligen Standort des Kaufes gültig."}  
+      {specialname:"Kostümevent",source:"assets/img/Kostümevent.jpg",description: "Beschere dir und allen anderen ein einzigartiges Erlebnis! Komme verkleidet mit Kostüm zu deinem Film und erhalte ein Gratis Menü."},
+                {specialname:"Popcornspecial",source:"assets/img/Popcornspecial.jpg",description:"Riesenpopcornangebot: Wenn du mit 4 weiteren Personen kommst, bekommt ihr einen großen Popcorneimer geschenkt."},
+                {specialname:"The Night Is Ours",source:"assets/img/movienight.jpg",description:"Filmmarathon über die ganze Nacht! Tauche mit vielen anderen in die fantastische Welt der Filme ein. Die Filme werden jeweils von den Zuschauern durch Abstimmungen bestimmt und ein Preis im Wert von 100€ lässt sich gewinnen! Die Tickets sind nur an der Kasse erhältlich und nur für den jeweiligen Standort des Kaufes gültig."}
               ];
 }
